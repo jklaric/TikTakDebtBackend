a559a3717b344cecf2bbc5e1f74f09b8c0c46889
Jakov <klaric9jakov@gmail.com>
2020-12-18 16:05:52 +0100

    Finished first 2 functions.

---
+++ TikTakDebtBackend/TikTakDebtBackend/Homework.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TikTakDebtBackend
+{
+    class Homework
+    {
+        public Homework(int id, string name, DateTime initialDay, DateTime finalDay, int daysExtended, int daysLate, string githubLink, string reviewLink)
+        {
+            ID = id;
+            Name = name;
+            InitialDay = initialDay;
+            FinalDay = finalDay;
+            DaysExtended = daysExtended;
+            DaysLate = daysLate;
+            GithubLink = githubLink;
+            ReviewLink = reviewLink;
+        }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime InitialDay { get; set; }
+
+        public DateTime FinalDay { get; set; }
+
+        public int DaysExtended { get; set; }
+
+        public int DaysLate { get; set; }
+
+        public string GithubLink { get; set; }
+
+        public string ReviewLink { get; set; }
+
+        public static Homework CreateNewHomework()
+        {
+            var isRunning = true;
+            var userInput = "";
+            var newHomeworkName = "";
+            var newHomeworkInitialDay = "";
+            var newHomeWorkFinalDay = "";
+            var newHomeworkDaysExtended = "";
+            var newHomeworkDaysLate = "";
+            var newHomeworkGithubLink = "";
+            var newHomeworkReviewLink = "";
+
+            while (isRunning)
+            {
+                Console.WriteLine("What is the name of the new homework?");
+                newHomeworkName = Console.ReadLine();
+
+                Console.WriteLine("When was the Homework given?");
+                newHomeworkInitialDay = Console.ReadLine();
+
+                Console.WriteLine("When is the deadline?");
+                newHomeWorkFinalDay = Console.ReadLine();
+
+                Console.WriteLine("How many days was the deadline extended?");
+                newHomeworkDaysExtended = Console.ReadLine();
+
+                Console.WriteLine("How many days after the deadline was the homework delivered?");
+                newHomeworkDaysLate = Console.ReadLine();
+
+                Console.WriteLine("What is the link to the homework?");
+                newHomeworkGithubLink = Console.ReadLine();
+
+                Console.WriteLine("What  is the link to the review?");
+                newHomeworkReviewLink = Console.ReadLine();
+
+                isRunning = false;
+            }
+
+            return new Homework(0, newHomeworkName, DateTime.Parse(newHomeworkInitialDay), DateTime.Parse(newHomeWorkFinalDay),
+                int.Parse(newHomeworkDaysExtended), int.Parse(newHomeworkDaysLate), newHomeworkGithubLink, newHomeworkReviewLink);
+        }
+    }
+}
---
+++ TikTakDebtBackend/TikTakDebtBackend/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TikTakDebtBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isRunning = true;
+            var homeworkList = new List<Homework>();
+            var userInput = "";
+
+            while (isRunning)
+            {
+                Console.WriteLine("Choose an action:");
+                Console.WriteLine("1 - Create a new homework tracker");
+                Console.WriteLine("2 - Print all homework trackers");
+                Console.WriteLine("3 - Edit a homework tracker");
+                Console.WriteLine("4 - Delete a homework tracker");
+                Console.WriteLine("0 - Exit program");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "0":
+                        while (isRunning)
+                        {
+                            Console.WriteLine("Do you really want to quit?");
+                            userInput = Console.ReadLine();
+
+                            if (userInput == "yes")
+                            {
+                                Console.Clear();
+                                Environment.Exit(0);
+                            }
+                            else if (userInput == "no")
+                            {
+                                Console.Clear();
+                                isRunning = false;
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Type yes or no.");
+                            }
+                        }
+                        isRunning = true;
+                        break;
+
+                    case "1":
+                        var newHomework = Homework.CreateNewHomework();
+                        homeworkList.Add(newHomework);
+                        newHomework.ID = homeworkList.Count;
+                        break;
+
+                    case "2":
+                        foreach (var homework in homeworkList)
+                        {
+                            var i = 1;
+                            Console.WriteLine(i + " - " + homework.Name + " from " + homework.InitialDay + " to " + homework.FinalDay);
+                            i++;
+                        }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Select homework:");
+                        foreach (var homework in homeworkList)
+                        {
+                            var i = 1;
+                            Console.WriteLine(i + " - " + homework.Name + " from " + homework.InitialDay + " to " + homework.FinalDay);
+                            i++;
+                        }
+
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Enter one of the valid numbers.");
+                        break;
+                }
+            }
+        }
+    }
+}
