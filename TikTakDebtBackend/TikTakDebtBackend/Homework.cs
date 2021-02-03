487dc2e489eeec9f27a2a840f076b563724d0c75
Jakov <klaric9jakov@gmail.com>
2021-02-03 17:49:01 +0100

    Finished input validation for main menu and first two functions.

--- TikTakDebtBackend/TikTakDebtBackend/Homework.cs
+++ TikTakDebtBackend/TikTakDebtBackend/Homework.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -48,30 +49,80 @@ namespace TikTakDebtBackend
 
             while (isRunning)
             {
+                Console.Clear();
                 Console.WriteLine("What is the name of the new homework?");
                 newHomeworkName = Console.ReadLine();
+                while (string.IsNullOrEmpty(newHomeworkName))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter a valid name.");
+                    newHomeworkName = Console.ReadLine();
+                }
 
+                Console.Clear();
                 Console.WriteLine("When was the Homework given?");
                 newHomeworkInitialDay = Console.ReadLine();
+                while (!DateTime.TryParse(newHomeworkInitialDay, out _))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please type the date in the format: YYYY-MM-DD hh:mm");
+                    newHomeworkInitialDay = Console.ReadLine();
+                }
 
+                Console.Clear();
                 Console.WriteLine("When is the deadline?");
                 newHomeWorkFinalDay = Console.ReadLine();
+                while (!DateTime.TryParse(newHomeWorkFinalDay, out _))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please type the date in the format: YYYY-MM-DD hh:mm");
+                    newHomeWorkFinalDay = Console.ReadLine();
+                }
 
+                Console.Clear();
                 Console.WriteLine("How many days was the deadline extended?");
                 newHomeworkDaysExtended = Console.ReadLine();
+                while (!int.TryParse(newHomeworkDaysExtended, out _))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter a valid number.");
+                    newHomeworkDaysExtended = Console.ReadLine();
+                }
 
+                Console.Clear();
                 Console.WriteLine("How many days after the deadline was the homework delivered?");
                 newHomeworkDaysLate = Console.ReadLine();
+                while (!int.TryParse(newHomeworkDaysLate, out _))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter a valid number.");
+                    newHomeworkDaysLate = Console.ReadLine();
+                }
 
+                Console.Clear();
                 Console.WriteLine("What is the link to the homework?");
                 newHomeworkGithubLink = Console.ReadLine();
+                while (string.IsNullOrEmpty(newHomeworkGithubLink))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter a valid link.");
+                    newHomeworkGithubLink = Console.ReadLine();
+                }
 
+                Console.Clear();
                 Console.WriteLine("What  is the link to the review?");
                 newHomeworkReviewLink = Console.ReadLine();
+                while (string.IsNullOrEmpty(newHomeworkReviewLink))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter a valid review link.");
+                    newHomeworkReviewLink = Console.ReadLine();
+                }
 
                 isRunning = false;
             }
 
+            Console.Clear();
             return new Homework(0, newHomeworkName, DateTime.Parse(newHomeworkInitialDay), DateTime.Parse(newHomeWorkFinalDay),
                 int.Parse(newHomeworkDaysExtended), int.Parse(newHomeworkDaysLate), newHomeworkGithubLink, newHomeworkReviewLink);
         }
--- TikTakDebtBackend/TikTakDebtBackend/Program.cs
+++ TikTakDebtBackend/TikTakDebtBackend/Program.cs
@@ -56,12 +56,14 @@ namespace TikTakDebtBackend
                         break;
 
                     case "1":
+                        Console.Clear();
                         var newHomework = Homework.CreateNewHomework();
                         homeworkList.Add(newHomework);
                         newHomework.Id = homeworkList.Count;
                         break;
 
                     case "2":
+                        Console.Clear();
                         foreach (var homework in homeworkList)
                         {
                             var finishedHomework = new FinishedHomework(homework.Id, homework.Name, homework.InitialDay, homework.FinalDay,
@@ -73,11 +75,14 @@ namespace TikTakDebtBackend
                         break;
 
                     case "3":
+                        Console.Clear();
                         EditHomework(homeworkList);
+                        Console.Clear();
                         break;
 
                     case "4":
                         DeleteHomework(homeworkList);
+                        Console.Clear();
                         break;
 
                     default:
@@ -91,9 +96,7 @@ namespace TikTakDebtBackend
         static void EditHomework(List<Homework> homeworkList)
         {
             var isRunning = true;
-            var selectedAction = "";
-            var isValidInput = false;
-
+            var editInput = "";
             Console.WriteLine("Choose an action:");
             Console.WriteLine("0 - Exit Process");
             Console.WriteLine("1 - Edit Name");
@@ -104,79 +107,136 @@ namespace TikTakDebtBackend
             Console.WriteLine("6 - Edit Github Link");
             Console.WriteLine("7 - Edit review Link");
 
-            while (isRunning)
+            var selectedAction = Console.ReadLine();
+            while (!int.TryParse(selectedAction, out _) || !(int.Parse(selectedAction) >= 0 && int.Parse(selectedAction) <= 7))
             {
+                Console.WriteLine("Please enter a valid number.");
                 selectedAction = Console.ReadLine();
+            }
 
-                for (int i = 0; i <= 7; i++)
-                {
-                    if (int.Parse(selectedAction) == 0)
-                    {
-                        return;
-                    }
-                    else if (int.Parse(selectedAction) == i)
-                    {
-                        isValidInput = true;
-                    }
-                }
-
-                if (!isValidInput)
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                }
-                else
-                {
-                    isRunning = false;
-                }
+            if (int.Parse(selectedAction) == 0)
+            {
+                return;
             }
 
             Console.WriteLine("Select homework:");
             foreach (var homework in homeworkList)
             {
                 var i = 1;
-                Console.WriteLine(i + " - " + homework.Name + " from " + homework.InitialDay + " to " +
-                                  homework.FinalDay);
+                Console.WriteLine(i + " - " + homework.Name + " from " + homework.InitialDay + " to " + homework.FinalDay);
                 i++;
             }
 
-            for (int i = 0; i < homeworkList.Count; i++)
+            var userInput = Console.ReadLine();
+            while (!int.TryParse(userInput, out _) || !(int.Parse(userInput) > 0 && int.Parse(userInput) < homeworkList.Count))
             {
-                var userInput = Console.ReadLine();
+                Console.WriteLine("Please enter a valid number.");
+                userInput = Console.ReadLine();
+            }
 
-                if (int.Parse(userInput) == i + 1)
+            while (isRunning)
+            {
+                Console.Clear();
+                switch (selectedAction)
                 {
-                    switch (selectedAction)
-                    {
-                        case "1":
-                            Console.WriteLine("What do you want to rename the homework to?");
-                            homeworkList[i].Name = Console.ReadLine();
-                            break;
-                        case "2":
-                            Console.WriteLine("What do you want the initial date changed to?");
-                            homeworkList[i].InitialDay = DateTime.Parse(Console.ReadLine());
-                            break;
-                        case "3":
-                            Console.WriteLine("What do you want the final date changed to?");
-                            homeworkList[i].FinalDay = DateTime.Parse(Console.ReadLine());
-                            break;
-                        case "4":
-                            Console.WriteLine("What do you want to change the days extended to?");
-                            homeworkList[i].DaysExtended = int.Parse(Console.ReadLine());
-                            break;
-                        case "5":
-                            Console.WriteLine("What do you want to change the days late to?");
-                            homeworkList[i].DaysLate = int.Parse(Console.ReadLine());
-                            break;
-                        case "6":
-                            Console.WriteLine("What do you want to change the github link to?");
-                            homeworkList[i].GithubLink = Console.ReadLine();
-                            break;
-                        case "7":
-                            Console.WriteLine("What do you want to change the review link to?");
-                            homeworkList[i].ReviewLink = Console.ReadLine();
-                            break;
-                    }
+                    case "1":
+                        Console.WriteLine("What do you want to rename the homework to?");
+                        editInput = Console.ReadLine();
+                        while (string.IsNullOrEmpty(editInput))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please enter a valid name.");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].Name = editInput;
+                        isRunning = false;
+                        break;
+
+                    case "2":
+                        Console.WriteLine("What do you want the initial date changed to?");
+                        editInput = Console.ReadLine();
+                        while (!DateTime.TryParse(editInput, out _))
+                        { 
+                            Console.Clear();
+                            Console.WriteLine("Please type the date in the format: YYYY-MM-DD hh:mm");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].InitialDay = DateTime.Parse(editInput);
+                        isRunning = false;
+                        break;
+
+                    case "3":
+                        Console.WriteLine("What do you want the final date changed to?");
+                        editInput = Console.ReadLine();
+                        while (!DateTime.TryParse(editInput, out _))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please type the date in the format: YYYY-MM-DD hh:mm");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].FinalDay = DateTime.Parse(editInput);
+                        isRunning = false;
+                        break;
+
+                    case "4":
+                        Console.WriteLine("What do you want to change the days extended to?");
+                        editInput = Console.ReadLine();
+                        while (!int.TryParse(editInput, out _))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please enter a valid number.");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].DaysExtended = int.Parse(editInput);
+                        isRunning = false;
+                        break;
+
+                    case "5":
+                        Console.WriteLine("What do you want to change the days late to?");
+                        editInput = Console.ReadLine();
+                        while (!int.TryParse(editInput, out _))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please enter a valid number.");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].DaysLate = int.Parse(editInput);
+                        isRunning = false;
+                        break;
+
+                    case "6":
+                        Console.WriteLine("What do you want to change the github link to?");
+                        editInput = Console.ReadLine();
+                        while (string.IsNullOrEmpty(editInput))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please enter a valid github link.");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].GithubLink = editInput;
+                        isRunning = false;
+                        break;
+
+                    case "7":
+                        Console.WriteLine("What do you want to change the review link to?");
+                        editInput = Console.ReadLine();
+                        while (string.IsNullOrEmpty(editInput))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please enter a valid review link.");
+                            editInput = Console.ReadLine();
+                        }
+                        homeworkList[int.Parse(userInput) - 1].ReviewLink = editInput;
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a valid number.");
+                        break;
                 }
+
+
+                return;
             }
         }
         static void DeleteHomework(List<Homework> homeworkList)
